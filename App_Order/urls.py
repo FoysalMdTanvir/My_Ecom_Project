e92b4f1dcdcc0419813da0c6e3d122b016701969
MdTanvirFoysal <tanvirfaisal54@gmail.com>
2021-02-15 13:45:54 +0600

    Increase and Decrease Quantity

--- App_Order/urls.py
+++ App_Order/urls.py
@@ -5,5 +5,8 @@ app_name = 'App_Order'
 
 urlpatterns = [
     path('add/<pk>/', views.add_to_cart, name="add"),
+    path('remove/<pk>/', views.remove_from_cart, name="remove"),
     path('cart/', views.cart_view, name="cart"),
+    path('increase/<pk>/', views.increase_cart, name="increase"),
+    path('decrease/<pk>/', views.decrease_cart, name="decrease"),
 ]
